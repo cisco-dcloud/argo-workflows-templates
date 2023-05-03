76e22dae59efbeb626baed0b2e671d54b8d1e6be
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-03 10:38:59 +0200

    remove cat

--- execute_wf.sh
+++ execute_wf.sh
@@ -1,6 +1,6 @@
 
 ARGO_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-JSON_WF=$(cat cat /var/mountpoint/argo-workflows-templates/workflows/namespaced_sandbox.json )
+JSON_WF=$(cat /var/mountpoint/argo-workflows-templates/workflows/namespaced_sandbox.json )
 
 curl \
    http://workflows-argo-workflows-server.argo:2746/api/v1/workflows/argo \
