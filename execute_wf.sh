9a7e54813c8c2a3c8375abc4dd133ab7a3375dab
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-03 10:36:29 +0200

    test

---
+++ execute_wf.sh
@@ -0,0 +1,9 @@
+
+ARGO_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+JSON_WF=$(cat cat /var/mountpoint/argo-workflows-templates/workflows/namespaced_sandbox.json )
+
+curl \
+   http://workflows-argo-workflows-server.argo:2746/api/v1/workflows/argo \
+  -H "Authorization: $ARGO_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -d \'$ARGO_TOKEN\'
