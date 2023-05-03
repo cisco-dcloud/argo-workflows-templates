a1fedd953b1334100e2a24413a68519143fe34c7
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-03 22:19:15 +0200

    cleanup

--- argo_env.sh
+++
@@ -1,7 +0,0 @@
-export ARGO_SERVER='workflows-argo-workflows-server.argo:2746' 
-export ARGO_HTTP1=true
-export ARGO_SECURE=false
-export ARGO_BASE_HREF=
-#export ARGO_TOKEN="Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-export ARGO_NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-export KUBECONFIG=/config/kubeconfig
--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
-kubectl config set-cluster cfc --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+export CLUSTER_SERVER="https://kubernetes.default"
+export CLUSTER_CA="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+export USER_TOKEN_VALUE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+export K8S_NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+export KUBECONFIG=/config/kubeconfig
+
+
+kubectl config set-cluster cfc --server=$CLUSTER_SERVER --certificate-authority=$CLUSTER_CA
 kubectl config set-context cfc --cluster=cfc
-kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+kubectl config set-credentials user --token=$USER_TOKEN_VALUE
 kubectl config set-context cfc --user=user
 kubectl config use-context cfc
 
-export K8S_NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-export USER_TOKEN_NAME=wc-sshserver
 export SA_NAME=wc-sshserver
-export USER_TOKEN_VALUE=$(kubectl -n $K8S_NAMESPACE get secret/${USER_TOKEN_NAME} -o=go-template='{{.data.token}}' | base64 --decode)
+#export USER_TOKEN_NAME=wc-sshserver
+#export USER_TOKEN_VALUE=$(kubectl -n $K8S_NAMESPACE get secret/${USER_TOKEN_NAME} -o=go-template='{{.data.token}}' | base64 --decode)
 export CURRENT_CONTEXT=$(kubectl config current-context)
-export CLUSTER_CA="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-export CLUSTER_SERVER="https://kubernetes.default"
 
-cat << EOF > /config/kubeconfig
+
+
+cat << EOF > $KUBECONFIG
 apiVersion: v1
 kind: Config
 current-context: ${CURRENT_CONTEXT}
@@ -40,5 +47,4 @@ export ARGO_HTTP1=true
 export ARGO_SECURE=false
 export ARGO_BASE_HREF=
 #export ARGO_TOKEN="Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-export ARGO_NAMESPACE=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)
-export KUBECONFIG=/config/kubeconfig
+export ARGO_NAMESPACE=$K8S_NAMESPACE
