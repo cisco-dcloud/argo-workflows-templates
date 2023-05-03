9633b7652bb8879206dc51c484683556f4af8e97
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-03 18:31:47 +0200

    update scripts

---
+++ kube_context.sh
@@ -0,0 +1,5 @@
+kubectl config set-cluster cfc --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+kubectl config set-context cfc --cluster=cfc
+kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+kubectl config set-context cfc --user=user
+kubectl config use-context cfc
