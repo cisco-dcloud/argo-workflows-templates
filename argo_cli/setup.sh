51822af27b0d99f64d857993dd3f3091df4af2d9
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-05 23:40:01 +0200

    update

--- argo_cli/setup.sh
+++ argo_cli/setup.sh
@@ -10,34 +10,33 @@ kubectl config set-credentials user --token=$USER_TOKEN_VALUE
 kubectl config set-context cfc --user=user
 kubectl config use-context cfc
 
-export SA_NAME=wc-sshserver
-export CURRENT_CONTEXT=$(kubectl config current-context)
+# export SA_NAME=wc-sshserver
+# export CURRENT_CONTEXT=$(kubectl config current-context)
 
+# cat << EOF > /config/kubeconfig
+# apiVersion: v1
+# kind: Config
+# current-context: ${CURRENT_CONTEXT}
+# contexts:
+# - name: ${CURRENT_CONTEXT}
+#   context:
+#     cluster: ${CURRENT_CONTEXT}
+#     user: $SA_NAME
+#     namespace: $K8S_NAMESPACE
+# clusters:
+# - name: ${CURRENT_CONTEXT}
+#   cluster:
+#     certificate-authority-data: ${CLUSTER_CA}
+#     server: ${CLUSTER_SERVER}
+# users:
+# - name: $SA_NAME
+#   user:
+#     token: ${USER_TOKEN_VALUE}
+# EOF
 
-cat << EOF > /config/kubeconfig
-apiVersion: v1
-kind: Config
-current-context: ${CURRENT_CONTEXT}
-contexts:
-- name: ${CURRENT_CONTEXT}
-  context:
-    cluster: ${CURRENT_CONTEXT}
-    user: $SA_NAME
-    namespace: $K8S_NAMESPACE
-clusters:
-- name: ${CURRENT_CONTEXT}
-  cluster:
-    certificate-authority-data: ${CLUSTER_CA}
-    server: ${CLUSTER_SERVER}
-users:
-- name: $SA_NAME
-  user:
-    token: ${USER_TOKEN_VALUE}
-EOF
-
-export ARGO_SERVER='workflows-argo-workflows-server.argo:2746' 
-export ARGO_HTTP1=true
-export ARGO_SECURE=false
-export ARGO_BASE_HREF=
-export ARGO_NAMESPACE=$K8S_NAMESPACE
-export KUBECONFIG=/config/kubeconfig
+# export ARGO_SERVER='workflows-argo-workflows-server.argo:2746' 
+# export ARGO_HTTP1=true
+# export ARGO_SECURE=false
+# export ARGO_BASE_HREF=
+# export ARGO_NAMESPACE=$K8S_NAMESPACE
+# export KUBECONFIG=/config/kubeconfig
