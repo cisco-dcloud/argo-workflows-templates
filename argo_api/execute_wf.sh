5bcbc960cdb2920cc41e17f1b499e1d75519ce8d
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-08 15:51:59 +0200

    adding create from api

--- argo_api/execute_wf.sh
+++
@@ -1,9 +0,0 @@
-
-ARGO_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-JSON_WF=$(cat /var/mountpoint/argo-workflows-templates/workflows/namespaced_sandbox.json )
-
-curl \
-   http://workflows-argo-workflows-server.argo:2746/api/v1/workflows/argo \
-  -H "Authorization: $ARGO_TOKEN" \
-  -H 'Content-Type: application/json' \
-  -d "$JSON_WF"
