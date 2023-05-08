1916f5efafe28ec40b3d186f349dddc62f67a54d
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-08 21:18:57 +0200

    fix curl

--- argo_api/deleteWorkflowTemplate.sh
+++ argo_api/deleteWorkflowTemplate.sh
@@ -1,44 +1,24 @@
 # Example:
-# createWorkflowTemplate.sh whalesay
+# deleteWorkflowTemplate.sh whalesay
 
 ARGO_TOKEN="Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 SERVICEACCOUNT=argo-workflows
 
-#Reference
-# {
-#   "namespace": "$NAMESPACE",
-#   "resourceKind": "string",
-#   "resourceName": "string",
-#   "submitOptions": {
-#     "annotations": "string",
-#     "dryRun": true,
-#     "entryPoint": "string",
-#     "generateName": "string",
-#     "labels": "string",
-#     "name": "string",
-#     "ownerReference": {
-#       "apiVersion": "string",
-#       "blockOwnerDeletion": true,
-#       "controller": true,
-#       "kind": "string",
-#       "name": "string",
-#       "uid": "string"
-#     },
-#     "parameters": [
-#       "string"
-#     ],
-#     "podPriorityClassName": "string",
-#     "priority": 0,
-#     "serverDryRun": true,
-#     "serviceAccount": "string"
-#   }
-# }
+TEMPLATENAME=$1
 
-PAYLOAD_JSON=$(yq -o=json '.' ../workflows/wt_$1.yaml)
-
+read -r -d '' CMD  << EOF
 curl -vvv -X DELETE \
-   http://workflows-argo-workflows-server.argo:2746/api/v1/workflow-templates/$NAMESPACE \
   -H "Authorization: $ARGO_TOKEN" \
   -H "Content-Type: application/json" \
-  -d $PAYLOAD_JSON
+  "http://workflows-argo-workflows-server.argo:2746/api/v1/workflow-templates/$NAMESPACE/$TEMPLATENAME" \
+EOF
+
+echo "CURL command is:"
+echo "-------------------------------------"
+echo
+echo $CMD
+echo
+echo "-------------------------------------"
+echo
+$CMD
--- argo_api/listWorkflowTemplates.sh
+++ argo_api/listWorkflowTemplates.sh
@@ -1,26 +1,17 @@
 # Example:
-# createWorkflowTemplate.sh whalesay
+# deleteWorkflowTemplate.sh whalesay
 
 ARGO_TOKEN="Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 SERVICEACCOUNT=argo-workflows
 
-#Prepare Payload
-PAYLOAD_JSON=$(yq -o=json '.' ../workflows/wt_$1.yaml)
-read -r -d '' PAYLOAD_JSON  << EOF
-{
-  "namespace": "$NAMESPACE",
-  "template": $PAYLOAD_JSON
-}
-EOF
+TEMPLATENAME=$1
 
-#Prepare CURL command
 read -r -d '' CMD  << EOF
-curl -vvv \
+curl -vvv -X GET \
   -H "Authorization: $ARGO_TOKEN" \
   -H "Content-Type: application/json" \
   "http://workflows-argo-workflows-server.argo:2746/api/v1/workflow-templates/$NAMESPACE" \
-  -d '$PAYLOAD_JSON' \
 EOF
 
 echo "CURL command is:"
