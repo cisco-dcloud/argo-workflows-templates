0d23a266865a4bdd694a879b2ac54e883cbecb2c
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-19 01:21:32 +0200

    vpshere

--- terraform_examples/vsphere/provider.tf
+++ terraform_examples/vsphere/provider.tf
@@ -1,5 +1,5 @@
 provider "vsphere" {
-  version              = "~> 1.10"
+  version              = "~> 1.4"
   user                 = var.vsphere_username
   password             = var.vsphere_password
   vsphere_server       = var.vsphere_servers[var.vsphere_datacentre]
