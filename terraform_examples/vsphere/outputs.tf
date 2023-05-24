6b7010488045596717bab116a54942c22b30d022
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-24 11:09:43 +0200

    output back

--- terraform_examples/vsphere/outputs.tf
+++ terraform_examples/vsphere/outputs.tf
@@ -1,21 +1,21 @@
 
-# output "VM" {
-#   value = tomap({
-#     for k, i in module.vm_creation : k => i.VM
-#   })
-# }
-
-# output "ip" {
-#   description = "default ip address of the deployed VM"
-#   value = tomap({
-#     for k, i in module.vm_creation : k => i.ip
-#   })
-# }
-
-
-output vm {
-  description = "VM"
-  value = vsphere_virtual_machine.vm
-  sensitive = true
+output "VM" {
+  value = tomap({
+    for k, i in module.vm_creation : k => i.VM
+  })
 }
+
+output "ip" {
+  description = "default ip address of the deployed VM"
+  value = tomap({
+    for k, i in module.vm_creation : k => i.ip
+  })
+}
+
+
+# output vm {
+#   description = "VM"
+#   value = vsphere_virtual_machine.vm
+#   sensitive = true
+# }
 
