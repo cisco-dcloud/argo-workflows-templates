5655894a05bb9853294a13a1afb121864525d54e
Manuel Garcia Sanes (MAC) <mgarcias@cisco.com>
2023-05-24 09:53:28 +0200

    adding vm to output

--- terraform_examples/vsphere/outputs.tf
+++ terraform_examples/vsphere/outputs.tf
@@ -1,13 +1,20 @@
 
-output "VM" {
-  value = tomap({
-    for k, i in module.vm_creation : k => i.VM
-  })
+# output "VM" {
+#   value = tomap({
+#     for k, i in module.vm_creation : k => i.VM
+#   })
+# }
+
+# output "ip" {
+#   description = "default ip address of the deployed VM"
+#   value = tomap({
+#     for k, i in module.vm_creation : k => i.ip
+#   })
+# }
+
+
+output vm {
+  description = "VM"
+  value = vsphere_virtual_machine.vm
 }
 
-output "ip" {
-  description = "default ip address of the deployed VM"
-  value = tomap({
-    for k, i in module.vm_creation : k => i.ip
-  })
-}
